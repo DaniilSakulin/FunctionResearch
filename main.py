beb44c5420aa005ca29d38223f60131a4444fdfd
Nikolay <merzlovnik@mail.ru>
2022-12-10 13:21:08 +0300

    recovered signal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pandas
 import numpy as numpy
+from numpy import nanmax
 from pandas import read_csv
 from matplotlib import pyplot
 from scipy.fftpack import fft
@@ -8,17 +9,17 @@ import pywt
 import pywt.data
 
 
-# №1
+# № 1
 def print_mathematical_expected_value(ser):
     print("Mathematical expected value ", ser.mean())
 
 
-# №2
+# № 2
 def print_dispersion(ser):
     print("Dispersion ", series.std())
 
 
-# №3
+# № 3
 def print_scope(ser):
     ser_max = ser.max()
     ser_min = ser.min()
@@ -27,13 +28,13 @@ def print_scope(ser):
     print("Scope", abs(ser_max - ser_min))
 
 
-# №4
+# № 4
 def plot_autocorrelation_plot(ser):
     pandas.plotting.autocorrelation_plot(ser).plot()
     pyplot.show()
 
 
-# №5
+# № 5
 def plot_fur_window(ser):
     X = fft(ser)
     N = len(X)
@@ -55,15 +56,36 @@ def plot_fur_window(ser):
 
 # № 6
 def plot_wavelet_haar(ser):
-    cA, cD = pywt.dwt(ser, "haar")
-    pyplot.plot(cA, label="approximation coefficients")
+    c_a, c_d = pywt.dwt(ser, "haar")
+    pyplot.plot(c_a, label="approximation coefficients")
     pyplot.show()
 
+
+# № 7
 def plot_filtered_signal(ser):
-    cA1, cD1 = pywt.dwt(ser, "db2", "smooth")
-    r_signal = pywt.idwt(cA1, cD1, 'db2', 'smooth')
+    thresh = 0.6 * nanmax(ser)
+    wavelet = "bior4.4"
+    mode = "per"
+    coeff = pywt.wavedec(ser, wavelet, level=5, mode=mode)
+    coeff[1:] = (pywt.threshold(i, value=thresh, mode="soft") for i in coeff[1:])
+    reconstructed_signal = pywt.waverec(coeff, wavelet, mode=mode)
+    pyplot.plot(reconstructed_signal)
+    pyplot.show()
+
+
+# № 8
+def plot_recovered_signal(ser):
+    r_signal = get_recovered_signal(ser)
     pyplot.plot(r_signal)
     pyplot.show()
+    return pandas.DataFrame(r_signal)
+
+
+def get_recovered_signal(ser):
+    c_a, c_d = pywt.dwt(ser, "db2", "smooth")
+    r_signal = pywt.idwt(c_a, c_d, 'db2', 'smooth')
+    return pandas.DataFrame(r_signal)
+
 
 def csv_check(ser):
     print(type(ser))
@@ -84,3 +106,4 @@ if __name__ == '__main__':
     # plot_fur_window(series)
     # plot_wavelet_haar(series)
     # plot_filtered_signal(series)
+    plot_recovered_signal(series)
